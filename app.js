185e5946ffd7c80c8baaea656fd035e104a5e5cd
muthu5200 <67819017+muthu5200@users.noreply.github.com>
2020-10-09 11:56:48 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -5,8 +5,38 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function(req, res){
-  res.send("Hello");
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
+app.use("view engine", "ejs");
+
+app.use(bodyposter.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/",  function(req, res){
+
+  var today = new Date();
+
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  var day = today.tolocaldatestring("en-us", options);
+
+  res.render("list",{  kindofDay: day,newListItems});
+
+});
+
+app.post("/", function(req, res){
+  var item = req.body.newItems;
+
+ items.push(item);
+
+  res.redirect("/");
+
+  res.render("list",{kindofDay: day, newListItems: item});
+
 });
 
 app.listen(3000, function(){
